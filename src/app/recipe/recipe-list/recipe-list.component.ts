26599d9bd7859219c9562c0e5aa6de062795fa0d
p.wolski@polsource.com <pa-wo97@o2.pl>
2020-01-26 16:54:20 +0100

    Add two receipes

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -9,6 +9,8 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe []= [new Recipe('Boiled Eggs', 'Using this recipe you can prepare delicious eggs', 'https://wszystkoojedzeniu.pl/site/assets/files/2914/jajko-gotowane.jpg')
   ,new Recipe('Eggs in swert', 'Using this recipe you can preapre egg like masterchef!', 'https://kuchnialidla.pl/img/img/Original/f73d9cdb41-jajko-w-koszulce.jpg') 
+  ,new Recipe('Minced chops', 'Using this receipe you can preapere delicious chops', 'https://bi.im-g.pl/im/e6/4a/11/z18132198ICR,Przepis-na-kotlety-mielone--Najlepsze--bo-bez-udzi.jpg') 
+  ,new Recipe('Chops', 'Using this receipe you can preapere delicious chops with potatoes','https://www.kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/schabowe_00.jpg')
 ];
 
   constructor() { }
